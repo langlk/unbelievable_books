7c200cc586bcf19e2a007aa5b6e1d5279d258f4f
Kelsey Langlois and LDevinMacKrell <kelsey+ldevinmackrell@>
2017-12-04 15:43:53 -0800

    Add additional cart and raincheck tests.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -7,16 +7,28 @@ describe Account do
   it { should validate_presence_of :user }
 
   describe "#cart" do
-    it "returns the order with status 'cart' associated with the account" do
+    it "returns a new cart order for account if none exists" do
       user = FactoryBot.create(:user)
       expect(user.account.cart.status).to eq("Cart")
+    end
+
+    it "returns the cart order associated with account if it exists" do
+      user = FactoryBot.create(:user)
+      user.account.cart
+      expect(Order.first).to eq(user.account.cart)
     end
   end
 
   describe '#rainchecked' do
-    it "returns the order marked raincheck associated with the account" do
+    it "returns a new rainchecked order for account if one does not exist" do
       user = FactoryBot.create(:user)
       expect(user.account.raincheck.raincheck).to eq(true)
+    end
+
+    it "returns the rainchecked order for account if it exists" do
+      user = FactoryBot.create(:user)
+      user.account.raincheck
+      expect(Order.first).to eq(user.account.raincheck)
     end
   end
 end
