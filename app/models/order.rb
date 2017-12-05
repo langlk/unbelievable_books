046a44081c693c11a7de13e58ccc1cb3d6f886aa
Kelsey Langlois and LDevinMacKrell <kelsey+ldevinmackrell@>
2017-12-05 08:52:49 -0800

    Update Order to delete all associated items when order is deleted.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ActiveRecord::Base
   has_many :order_items
   validates_presence_of :account, :price_total, :status
   before_save :update_total
+  before_destroy :remove_items
 
   def add_total
     OrderItem.where(order: self).collect { |item| item.reserved_price * item.quantity }.sum
@@ -14,5 +15,11 @@ class Order < ActiveRecord::Base
 
   def book_count
     self.order_items.collect { |item| item.quantity }.sum
+  end
+
+  def remove_items
+    OrderItem.where(order: self).each do |item|
+      item.destroy
+    end
   end
 end
