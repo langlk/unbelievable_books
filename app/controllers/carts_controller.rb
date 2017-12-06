8cd688f9a0529e13638899b2efb8990facaced9f
Kelsey Langlois and LDevinMacKrell <kelsey+ldevinmackrell@>
2017-12-06 15:52:19 -0800

    Add currency service to cart page.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,5 +2,6 @@ class CartsController < ApplicationController
   def show
     @cart = current_cart
     @rainchecked = current_raincheck
+    @quotes = JSON.parse(Currency.get_exchange_rates)['quotes']
   end
 end
--- app/services/currency.rb
+++ app/services/currency.rb
@@ -1,6 +1,5 @@
 class Currency
-  def self.get_exchange_rate
-    response = RestClient::Request.execute(method: :get, url: "http://apilayer.net/api/live?access_key=#{ENV["CURRENCYLAYERKEY"]}&currencies=AUD,EUR,GBP,PLN")
-
+  def self.get_exchange_rates
+    response = RestClient::Request.execute(method: :get, url: "http://apilayer.net/api/live?access_key=#{ENV["CURRENCYLAYERKEY"]}&currencies=CAD,EUR,GBP,PLN")
   end
 end
